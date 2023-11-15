a2b64cbcadf27ed2cdbf0b87afbf147d3114fb94
mohamedasri44 <mohamedasri1810@gmail.com>
2023-11-15 17:07:54 +0100

    v3

--- routes/admin.php
+++ routes/admin.php
@@ -13,5 +13,3 @@ Route::get('dashboard',[AdminController::class,'dashboard'])->name('dashboard');
 Route::get('profile',[ProfileController::class,'index'])->name('profile');
 Route::post('profile/update',[ProfileController::class,'updateProfile'])->name('profile.update');
 Route::post('profile/update/password',[ProfileController::class,'updatePassword'])->name('password.update');
-/** profile routes */
-Route::post('profile/update/password',[ProfileController::class,'updatePassword'])->name('password.update');
